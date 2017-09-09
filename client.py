24749abd890fa2ece2b1a7b18318ed4b8bef4f4f
zhangsheng377 <435878393@qq.com>
2017-09-10 03:33:41 +0800

    另开了一个tcp专门来传图片

--- client.py
+++ client.py
@@ -2,19 +2,19 @@ import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(("192.168.31.240", 5007))
+file_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+file_client_socket.connect(("192.168.31.240", 5008))
 
 img = open("dog.jpg", 'rb')
-string = "".encode()
 while True:
-    strng = img.readline()
-    if not strng:
+    string = img.read(512)
+    if not string:
         break
-    string += strng
+    file_client_socket.send(string)
 img.close()
-if string:
-    client_socket.send(string)
-    print("Data sent successfully")
-    string = client_socket.recv(1024)
-    print(string)
+file_client_socket.close()
+print("Data sent successfully")
+string = client_socket.recv(1024)
+print(string)
 client_socket.close()
 exit()
--- server.py
+++ server.py
@@ -7,15 +7,26 @@ meta = dn.load_meta("cfg/coco.data".encode())
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("", 5007))
 server_socket.listen(5)
+file_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+file_socket.bind(("", 5008))
+file_socket.listen(5)
 
 while True:
     client_socket, address = server_socket.accept()
     print("Conencted to - ", address)
-    string = client_socket.recv()
-    if string:
-        fp = open("tmp.jpg", 'wb')
+    file_client_socket, address = server_socket.accept()
+    print("File Conencted to - ", address)
+    isEmpty = True
+    fp = open("tmp.jpg", 'wb')
+    while True:
+        string = file_client_socket.recv(512)
+        if not string:
+            break
         fp.write(string)
-        fp.close()
+        isEmpty = False
+    fp.close()
+    file_client_socket.close()
+    if not isEmpty:
         print("Data Received successfully")
         r = dn.detect(net, meta, "tmp.jpg".encode())
         print(r)
