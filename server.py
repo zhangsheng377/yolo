b83a9d3de0dbea2d058bda7c1832642c91be9f41
zhangsheng377 <435878393@qq.com>
2017-09-09 22:27:44 +0800

    测试调用关系

---
+++ server.py
@@ -0,0 +1,27 @@
+import socket
+
+'''server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("", 5005))
+server_socket.listen(5)
+import os
+
+
+
+while (1):
+    client_socket, address = server_socket.accept()
+    print("Conencted to - ", address, "\n")
+    fp = open("tmp.jpg", 'wb')
+    while True:
+        str = client_socket.recv(512)
+        if not str:
+            break
+        fp.write(str)
+    fp.close()
+    print("Data Received successfully")
+'''
+import darknet as dn
+
+net = dn.load_net("cfg/yolo.cfg", "yolo.weights", 0)
+meta = dn.load_meta("cfg/coco.data")
+r = dn.detect(net, meta, "data/dog.jpg")
+print(r)
